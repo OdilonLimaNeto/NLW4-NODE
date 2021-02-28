4fa6a35408d57402b04e9134d430af407adb9c3f
OdilonLimaNeto <odilongeronimoo@gmail.com>
2021-02-27 23:52:36 -0500

    NLW4 - test User OK - (27-02-2021)

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,10 +1,23 @@
 import request from 'supertest';
 import { app } from '../app';
 
-describe('Users', () => {
-    request(app).post('/users')
-    .send({
-        email: 'odilonlima@gmail.com',
-        name: 'Odilon Lima'
+import createConnection from '../database';
+
+describe('Users',  () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
     });
+    
+it('Should be able to create a new user', async () => { 
+    const response = await request(app).post('/users')
+        .send({
+            email: 'odilonlima@gmail.com',
+            name: 'Odilon Lima'
+        });
+
+        expect(response.status).toBe(201)
+    });
+
 });
+
